6e37bee386ac8c61538e5464a5254aeda59e52e2
Jeremy Dostie <jeremydo100@gmail.com>
2023-11-08 17:36:42 -0500

    initial interface

--- app/src/main/java/ca/csfoy/tp2/views/MainActivity.java
+++ app/src/main/java/ca/csfoy/tp2/views/MainActivity.java
@@ -3,14 +3,29 @@ package ca.csfoy.tp2.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import ca.csfoy.tp2.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button playButton;
+    private Button replayButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.playButton = (Button) findViewById(R.id.playButton);
+        this.replayButton = (Button) findViewById(R.id.replayButton);
+
     }
+
+    public void onPlayButton(){
+
+    }
+    public void onReplayButton(){
+
+    }
+
 }
