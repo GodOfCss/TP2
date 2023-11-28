921247652b1082bec52972e96fe0d805d83b1e6f
Jeremy Dostie <jeremydo100@gmail.com>
2023-11-28 09:44:49 -0500

    Etape 1

--- app/src/main/java/ca/csfoy/tp2/mc/GoModelController.java
+++ app/src/main/java/ca/csfoy/tp2/mc/GoModelController.java
@@ -4,20 +4,75 @@ import java.util.ArrayList;
 
 public class GoModelController {
     private boolean isBlackNext;
-    private ArrayList<String> playedSpots;
+    private ArrayList<Integer> playedBlackSpots;
+    private ArrayList<Integer> playedWhiteSpots;
+    private int lastPlayed;
+
 
     public GoModelController(){
         this.isBlackNext = true;
+        this.playedWhiteSpots = new ArrayList<Integer>();
+        this.playedBlackSpots = new ArrayList<Integer>();
     }
 
-    public void play(){
+    public void play(int id){
+        if(isBlackNext) {
+            this.isBlackNext = false;
+            this.playedBlackSpots.add(id);
+            this.lastPlayed = id;
+        }
+        else {
+            this.isBlackNext = true;
+            this.playedWhiteSpots.add(id);
+            this.lastPlayed = id;
+        }
 
-        if(isBlackNext) this.isBlackNext = false;
-        else this.isBlackNext = true;
+
     }
 
     public boolean isBlackNext() {
         return isBlackNext;
     }
 
+    public boolean isPlayed(int id) {
+        if(playedWhiteSpots.contains(id) || playedBlackSpots.contains(id)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBlack(int id){
+        if(playedBlackSpots.contains(id)){
+            return true;
+        }
+        return false;
+    }
+
+    public int getLastPlayed(){
+        return this.lastPlayed;
+    }
+
+    public ArrayList<Integer> getPlayedWhiteSpots() {
+        return this.playedWhiteSpots;
+    }
+
+    public ArrayList<Integer> getPlayedBlackSpots() {
+        return this.playedBlackSpots;
+    }
+
+    public void setNext(boolean isBlackNext) {
+        this.isBlackNext = isBlackNext;
+    }
+
+    public void setLastPlayed(int lastPlayed) {
+        this.lastPlayed = lastPlayed;
+    }
+
+    public void setWhitePlayed(ArrayList<Integer> playedWhite) {
+        this.playedWhiteSpots = playedWhite;
+    }
+
+    public void setBlackPlayed(ArrayList<Integer> playedBlack) {
+        this.playedBlackSpots = playedBlack;
+    }
 }
--- app/src/main/java/ca/csfoy/tp2/views/GameActivity.java
+++ app/src/main/java/ca/csfoy/tp2/views/GameActivity.java
@@ -1,5 +1,6 @@
 package ca.csfoy.tp2.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class GameActivity extends AppCompatActivity implements GoView {
     private ImageButton[][] goBoard = new ImageButton[GAME_DIMENSIONS][GAME_DIMENSIONS];
 
     private Button playButton;
+    private ImageButton currentPlay;
     private GoModelController controller = new GoModelController();
 
     @Override
@@ -24,26 +26,83 @@ public class GameActivity extends AppCompatActivity implements GoView {
         setContentView(R.layout.game_activity);
 
         this.playButton = findViewById(R.id.playButton);
+        playButton.setOnClickListener(this::OnPlay);
 
         for(int i = 0; i < GAME_DIMENSIONS; i++){
-            for(int j = 1; j <= GAME_DIMENSIONS; j++){
+            for(int j = 1; j <= GAME_DIMENSIONS; j++) {
                 String currentButtonString = "go_" + LETTERS[i] + Integer.toString(j);
                 int id = getResources().getIdentifier(currentButtonString, "id", getPackageName());
-                this.goBoard[i][j-1] = (ImageButton)findViewById(id);
-                goBoard[i][j-1].setOnClickListener(this::OnClickBoard);
+                this.goBoard[i][j - 1] = (ImageButton) findViewById(id);
+                goBoard[i][j - 1].setOnClickListener(this::OnClickBoard);
             }
+        }
+
+        if(savedInstanceState != null){
+            controller.setNext(savedInstanceState.getBoolean("IS_BLACK_NEXT"));
+            controller.setLastPlayed(savedInstanceState.getInt("LAST_PLAYED"));
+            controller.setWhitePlayed(savedInstanceState.getIntegerArrayList("PLAYED_WHITE"));
+            controller.setBlackPlayed(savedInstanceState.getIntegerArrayList("PLAYED_BLACK"));
+
         }
 
     }
 
-    private void OnClickBoard(View view) {
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putBoolean("IS_BLACK_NEXT",controller.isBlackNext());
+        outState.putIntegerArrayList("PLAYED_WHITE", controller.getPlayedWhiteSpots());
+        outState.putIntegerArrayList("PLAYED_BLACK", controller.getPlayedBlackSpots());
+        outState.putInt("LAST_PLAYED", controller.getLastPlayed());
+        super.onSaveInstanceState(outState);
+    }
 
+    private void OnPlay(View view) {
+        if(currentPlay != null){
+            controller.play(currentPlay.getId());
+        }
+        this.update();
+
+    }
+
+    private void OnClickBoard(View view) {
+        this.update();
+        ImageButton currentButton = (ImageButton) view;
+        int id = currentButton.getId();
+        if(controller.isBlackNext()){
+            currentButton.setImageResource(R.mipmap.go_black_piece);
+        }
+        else{
+            currentButton.setImageResource(R.mipmap.go_white_piece);
+        }
+
+        this.currentPlay = currentButton;
     }
 
     public void update() {
         for(int i = 0; i < GAME_DIMENSIONS; i++){
-            for(int j = 1; j <= GAME_DIMENSIONS; j++){
+            for(int j = 0; j < GAME_DIMENSIONS; j++){
+                int currentId = goBoard[i][j].getId();
+                if(controller.isPlayed(currentId)) {
+                    if(controller.isBlack(currentId)){
+                        goBoard[i][j].setImageResource(R.mipmap.go_black_piece);
+                    }
+                    else{
+                        goBoard[i][j].setImageResource(R.mipmap.go_white_piece);
+                    }
+                }
+                else{
+                    goBoard[i][j].setImageDrawable(null);
+                }
+            }
+        }
 
+        int lastID = controller.getLastPlayed();
+        ImageButton lastPlayed = findViewById(lastID);
+        if(lastPlayed != null) {
+            if (controller.isBlack(lastID)) {
+                lastPlayed.setImageResource(R.mipmap.go_black_piece_play);
+            } else {
+                lastPlayed.setImageResource(R.mipmap.go_white_piece_play);
             }
         }
     }
